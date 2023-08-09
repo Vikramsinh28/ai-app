26157dc7dfd75c835a1b23d2802005cf9e2b1146
Vikramsinh28 <vikramsinhparmar2812@gmail.com>
2023-08-10 01:03:54 +0530

    ai-web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+import { Box, TextField } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
+import { SendAndArchiveOutlined } from "@mui/icons-material";
+
+const ClearIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+    </svg>
+  );
+};
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleClear = () => {
+    setValue("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout>
+        <Box
+          sx={{ background: "#f7f7ff" }}
+          style={{
+            margin: "0%",
+            borderRadius: "20px",
+            height: "100vh",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+
+
+          </Box>
+          <Box  
+            style={{
+              position : 'sticky',
+              bottom : 0,
+              margin : '0 20%',
+              display : 'flex'
+            }}
+
+          >
+            <TextField
+              label="Your text"
+              value={value}
+              onChange={handleChange}
+              style={{
+                width: "100%",
+                border: 0,
+                backgroundColor: "#fff",
+              }}
+            />
+            <IconButton
+              onClick={handleClear}
+              disabled={value === ""}
+              style={{ marginLeft: 10 }}
+            >
+                <SendAndArchiveOutlined />
+            </IconButton>
+          </Box>
+        </Box>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,224 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Button, Stack } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Info, Message } from '@mui/icons-material';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+export default function MiniDrawer({children}) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open} sx ={{
+         boxShadow : 'none',
+         borderBottom : '1px solid #e0e0e0',
+      }}>
+        <Toolbar
+            sx={{   
+                backgroundColor : '#fff',
+                boxShadow : 'none', 
+            }}
+        >
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+              color : '#000'
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" sx={{
+                color : '#000',
+          }}>
+            AI WEB
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+
+        <Stack sx={{
+          width: '100%',
+          display : 'flex',
+          justifyContent : 'center',
+          alignItems : 'center',
+          mt : 2
+        }}
+         style={{
+            cursor : 'pointer'
+         }}
+        >
+          <Button
+            sx={{
+              width: '50%',
+              borderRadius: 4,
+              backgroundColor : '#f5f5f5',
+              height: 40,
+              color: '#0e0e0e',
+              '&:hover': {
+              },
+            }}
+
+            style={{
+              margin : '0 20%'
+            }}
+          >
+            <AddIcon sx={{ mr: 1 ,
+              color : '#0e0e0e',
+              fontSize : 20
+            }} />
+          </Button>
+          
+        </Stack>
+
+        <List>
+          {['Chat 1', 'Chat 2'].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                  display   : 'flex',
+                  alignItems : 'center',
+                  justifyContent : 'center'
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                > 
+                  <Message sx={{ width: 20, height: 20 }} />
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                
+                <Info sx={{ width: 20, height: 20 }} style = {{
+                  position : 'absolute',
+                  right : 10,
+                  color : 'grey'
+                }}
+                  onClick={() => {
+                    alert('info')
+                  }}
+                />
+
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List> 
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        {children}
+      </Box>
+    </Box>
+  );
+}
