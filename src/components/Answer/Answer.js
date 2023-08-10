30efd5c82c4a1df3238eca0bb9fea23b5c009397
vikramp <vikram.parmar@cybercom.in>
2023-08-10 16:13:07 +0530

    animation

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,51 +1,91 @@
-import { Avatar, Container, Stack, Typography } from '@mui/material';
-import React from 'react';
+import { Avatar, Container, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import FlareIcon from "@mui/icons-material/Flare";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  loading: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  rotatingIcon: {
+    animation: "$spin 1s linear infinite",
+  },
+  "@keyframes spin": {
+    "0%": {
+      transform: "rotate(0deg)",
+    },
+    "100%": {
+      transform: "rotate(360deg)",
+    },
+  },
+}));
 
 const AnswerDisplay = ({ question, answer }) => {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a 3-second delay before showing the answer
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Container  style={{
+    <Container
+      style={{
         float: "left",
-        marginTop : "3vh",
-    }}>
-        <Stack spacing={2} >
-            <Stack direction="row" spacing={2} style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                top: 0,
-            }}>
-
-                <Typography variant="h6" component="div" gutterBottom >
-                        <Avatar sx={{ bgcolor: '#0e0e0e', width: 24, height: 24, fontSize: 12 }}></Avatar>
-                </Typography>
-                <Typography variant="h6" component="div" gutterBottom>
-                        {question}
-                </Typography>
-            
-            </Stack>
-
-            <Stack direction="row" spacing={2} style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                top: 0,
-            }}>
-
-                <Typography variant="h6" component="div" gutterBottom>
-                        {answer}
-                </Typography>
-            
-            </Stack>
-
+        marginTop: "3vh",
+      }}
+    >
+      <Stack spacing={2}>
+        <Stack
+          direction="row"
+          spacing={2}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            top: 0,
+          }}
+        >
+          <Typography variant="h6" component="div" gutterBottom>
+            <Avatar
+              sx={{ bgcolor: "#0e0e0e", width: 24, height: 24, fontSize: 12 }}
+            ></Avatar>
+          </Typography>
+          <Typography variant="h6" component="div" gutterBottom>
+            {question}
+          </Typography>
         </Stack>
 
+        <Stack
+          direction="row"
+          spacing={2}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            top: 0,
+          }}
+        >
+          {loading ? (
+            <div className={classes.loading}>
+              <FlareIcon className={classes.rotatingIcon} />
+            </div>
+          ) : (
+            <Typography variant="body1">{answer}</Typography>
+          )}
+        </Stack>
+      </Stack>
     </Container>
   );
 };
 
 export default AnswerDisplay;
-
-
-
